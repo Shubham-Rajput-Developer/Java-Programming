0c8edbb834f889ce1ed6d26f71cab1614ddfccbc
Shubham-Rajput-Developer <shubhamrajput0504@gmail.com>
2025-01-07 00:00:54 +0530

    New File

--- JavaBasic.java
+++ JavaBasic.java
@@ -8,14 +8,39 @@ public class JavaBasic {
         // System.out.println(a);
 
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a: ");
+        // System.out.print("Enter a: ");
+        // int a = sc.nextInt();
+        // System.out.print("Enter b: ");
+        // int b = sc.nextInt();
+        // int sum = a+b;
+        // int product = a*b;
+        // System.out.println("Sum is : "+sum);
+        // System.out.println("Product is : "+product);
+
+        // //Area of circle
+        // System.out.println("Enter radius : ");
+        // float r = sc.nextFloat();
+        // float area = 3.14f*r*r;
+        // System.out.println("Area of circle is : "+area);
+
+        //Practice qustions
+
+        System.out.print("Enter no 1 : ");
         int a = sc.nextInt();
-        System.out.print("Enter b: ");
+        System.out.print("Enter no 2 : ");
         int b = sc.nextInt();
-        int sum = a+b;
-        int product = a*b;
-        System.out.println("Sum is : "+sum);
-        System.out.println("Product is : "+product);
+        System.out.print("Enter no 3 : ");
+        int c = sc.nextInt();
+        int avg = (a+b+c)/3;
+        double totalWithGst = (a + b + c) * (1 + 18.0 / 100);
+        System.out.println("Average is : "+avg);
+        System.out.println("Total cost with GST : "+totalWithGst);
+
+        // System.out.println("Enter side : ");
+        // int side = sc.nextInt();
+        // int area = side*side;
+        // System.out.println("Area of sqaure is : "+area);
+
     }
 }
 
