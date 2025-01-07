491d165548b2105a981c25159f1fed5726f49d7d
Shubham-Rajput-Developer <shubhamrajput0504@gmail.com>
2025-01-07 23:06:20 +0530

    Changes

--- JavaBasic.java
+++ JavaBasic.java
@@ -25,22 +25,35 @@ public class JavaBasic {
 
         //Practice qustions
 
-        System.out.print("Enter no 1 : ");
-        int a = sc.nextInt();
-        System.out.print("Enter no 2 : ");
-        int b = sc.nextInt();
-        System.out.print("Enter no 3 : ");
-        int c = sc.nextInt();
-        int avg = (a+b+c)/3;
-        double totalWithGst = (a + b + c) * (1 + 18.0 / 100);
-        System.out.println("Average is : "+avg);
-        System.out.println("Total cost with GST : "+totalWithGst);
+        // System.out.print("Enter no 1 : ");
+        // int a = sc.nextInt();
+        // System.out.print("Enter no 2 : ");
+        // int b = sc.nextInt();
+        // System.out.print("Enter no 3 : ");
+        // int c = sc.nextInt();
+        // int avg = (a+b+c)/3;
+        // double totalWithGst = (a + b + c) * (1 + 18.0 / 100);
+        // System.out.println("Average is : "+avg);
+        // System.out.println("Total cost with GST : "+totalWithGst);
 
         // System.out.println("Enter side : ");
         // int side = sc.nextInt();
         // int area = side*side;
         // System.out.println("Area of sqaure is : "+area);
 
+        System.out.println("Enter a and b : ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        // System.out.println("Addition is : "+(a+b));
+        // System.out.println("Subtraction is : "+(a-b));
+        // System.out.println("Modulo is : "+(a%b));
+        // System.out.println("Unary operator (Pre increment) : "+(++a));
+        // System.out.println("Unary operator (Post increment) : "+(b++));
+        // System.out.println("a equals b o not : "+(a==b));
+        // System.out.println("a equals b or not : "+(a!=b));
+        b += a;
+        System.out.println(b);
+        
     }
 }
 
