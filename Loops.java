aaeede5ccc417eae1e578de72a523c13bb4652f7
Shubham-Rajput-Developer <shubhamrajput0504@gmail.com>
2025-01-15 13:54:58 +0530

    DSA

---
+++ Loops.java
@@ -0,0 +1,55 @@
+import java.util.*;
+public class Loops{
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+    //   System.out.print("Enter Print count : ");
+    //   int count = sc.nextInt();
+    //   int n = 1;
+    //   while(n<count){
+    //     System.out.println("Shubham Rajput->CDS");
+    //     n++;
+    //   }  
+    // while (n<=10) {
+    //     System.out.println(n);
+    //     n++;
+    // }
+    // while(n<=count){
+    //     System.out.println(n);
+    //     n++;
+    // }
+
+    //Sum of N Natural number
+    // int sum = 0;
+    // while(n<=count){
+    //     sum += n;
+    //     n++;
+    // }
+    // System.out.println("Sum is : "+sum);
+    
+        // for(int i=1;i<count;i++){
+        //     System.out.println(i);
+        // }
+    
+    //Print sqaure pattern
+    // for(int i=1;i<=4;i++){
+    //     System.out.println("****");
+    // }
+
+    do{
+        System.out.println("Enter your Number : ");
+        int n = sc.nextInt();
+        if(n%10==0){
+            // break;
+            continue;
+        }
+        System.out.println("Number was : "+n);
+    }while(true);
+
+    // for(int i=0;i<10;i++){
+    //     if(i==3){
+    //         continue;
+    //     }
+    //     System.out.println(i);
+    // }
+    }
+}
