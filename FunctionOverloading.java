6d61786973ac3bf1576d0cfef8d6b7f4a56ea4d7
Shubham-Rajput-Developer <shubhamrajput0504@gmail.com>
2025-01-16 13:12:22 +0530

    functions

---
+++ FunctionOverloading.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class FunctionOverloading {
+
+    public static int Sum(int a, int b){
+        return a+b;
+    }
+    
+    public static int Sum(int a, int b, int c){
+        return a+b+c;
+    }
+
+    public static float Sum(float a, float b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Num 1 : ");
+        int a = sc.nextInt();
+        System.out.print("Enter Num 2 : ");
+        int b = sc.nextInt();
+        System.out.print("Enter Num 3 : ");
+        int c = sc.nextInt();
+
+        System.out.println(Sum(a,b));
+        System.out.println(Sum(a,b,c));
+        System.out.println(Sum(4.4f,5.5f));
+    }
+}
---
+++ JavaFunctions.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class JavaFunctions {
+    public static int Product(int a, int b){
+        return a*b;
+    }
+
+    public static int Factorial(int a){
+        int fact = 1;
+        for(int i=a;i>0;i--){
+            fact = fact*i;
+        }
+        return fact;
+    }
+
+    public static int BinomialCoficient(int n, int r){
+        int result = Factorial(n)/(Factorial(r)*Factorial(n-r));
+        return result;
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Num 1 : ");
+        int a = sc.nextInt();
+        System.out.print("Enter Num 2 : ");
+        int b = sc.nextInt();
+
+        System.out.println("Product is : "+Product(a, b));
+        
+        System.out.println("Factorial of Num a is : "+Factorial(a));
+        
+        System.out.println("Binomial coificient of a and b i s: "+BinomialCoficient(a, b));
+        
+        
+    }
+}
