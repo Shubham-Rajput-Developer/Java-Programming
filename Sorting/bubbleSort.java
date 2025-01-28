9e20d46a8c5b62c0ea8a6e30f9ec192e20d55141
Shubham-Rajput-Developer <shubhamrajput0504@gmail.com>
2025-01-28 23:35:22 +0530

    Done

--- Sorting/bubbleSort.java
+++ Sorting/bubbleSort.java
@@ -1,7 +1,7 @@
 public class bubbleSort{
     public static void Sort(int nums[]){
-        for(int i=0;i<nums.length-1;i++){
-            for(int j=0;j<nums.length-1-i;j++){
+        for(int i=1;i<nums.length;i++){
+            for(int j=0;j<nums.length-i;j++){
                 if(nums[j]>nums[j+1]){
                     int temp = nums[j];
                     nums[j] = nums[j+1];
