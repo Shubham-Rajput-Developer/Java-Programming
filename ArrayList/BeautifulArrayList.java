80dd4f35f02a1f2d82d9f94e9f258fc6e225a717
Shubham-Rajput-Developer <shubhamrajput0504@gmail.com>
2025-07-30 00:06:43 +0530

    Beautiful array

--- ArrayList/BeautifulArrayList.java
+++ ArrayList/BeautifulArrayList.java
@@ -2,35 +2,50 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class BeautifulArrayList {
-    public static ArrayList<Integer> makeBeautiful(int num){
+    // public static ArrayList<Integer> makeBeautiful(int num){
 
-        ArrayList<Integer> ans = new ArrayList<>();
-        ans.add(1);
-        while(ans.size()<num){
-            ArrayList<Integer> temp = new ArrayList<>();
+    //     ArrayList<Integer> ans = new ArrayList<>();
+    //     ans.add(1);
+    //     while(ans.size()<num){
+    //         ArrayList<Integer> temp = new ArrayList<>();
 
-            //for odd numbers
-            for(int i=0;i<ans.size();i++){
-                if(ans.get(i)*2-1<=num){
-                    temp.add(ans.get(i)*2-1);
-                }
-            }
+    //         //for odd numbers
+    //         for(int i=0;i<ans.size();i++){
+    //             if(ans.get(i)*2-1<=num){
+    //                 temp.add(ans.get(i)*2-1);
+    //             }
+    //         }
 
-            //for even numbers
-            for(int i=0;i<ans.size();i++){
-                if(ans.get(i)*2<=num){
-                    temp.add(ans.get(i)*2);
-                }
-            }
-            ans = temp;
-        }
-        return ans;
+    //         //for even numbers
+    //         for(int i=0;i<ans.size();i++){
+    //             if(ans.get(i)*2<=num){
+    //                 temp.add(ans.get(i)*2);
+    //             }
+    //         }
+    //         ans = temp;
+    //     }
+    //     return ans;
+    // }
+
+    public static ArrayList<Integer> optimized(int n){
+        ArrayList<Integer> res = new ArrayList<>();
+        divideConque(1,1,res,n);
+        return res;
     }
+    public static void divideConque(int start, int inc, ArrayList<Integer> res,int n){
+        if(start+inc>n){
+            res.add(start);
+            return;
+        }
+        divideConque(start, 2*inc, res, n);
+        divideConque(start+inc, 2*inc, res, n);
+    }
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter range: ");
         int num = sc.nextInt();
-        System.out.println(makeBeautiful(num));
+        System.out.println(optimized(num));
         sc.close();
     }
 }
