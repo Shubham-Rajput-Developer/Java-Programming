53f40c1759e84fbbad93799e97410bb02b0018fa
Shubham-Rajput-Developer <shubhamrajput0504@gmail.com>
2025-08-05 23:38:33 +0530

    Stack

---
+++ Stack/ReverseStack.java
@@ -0,0 +1,40 @@
+import java.util.Stack;
+
+public class ReverseStack { 
+
+    public static void PushAtBottom(Stack<Integer> s, int data){
+        if(s.isEmpty()){
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        PushAtBottom(s, data);
+        s.push(top);
+    }
+
+    public static void reverse(Stack<Integer> s){
+        if(s.isEmpty()){
+            return;
+        }
+        int top = s.pop();
+        reverse(s);
+        PushAtBottom(s,top);
+    }
+
+    public static void printStack(Stack<Integer> s){
+        while(!s.isEmpty()){
+            System.out.print(s.pop()+" ");
+        }
+    }
+
+    public static void main(String args[]){
+        Stack<Integer> s = new Stack<>();
+        s.push(3);
+        s.push(2);
+        s.push(1);
+
+        reverse(s);
+        printStack(s);
+    }
+    
+}
