ba3e0d7d1f66c9b7ff3e47b98a16ae6a0e11adea
Shubham-Rajput-Developer <shubhamrajput0504@gmail.com>
2025-10-02 00:22:10 +0530

    Greedy Algorithm

---
+++ GreedyAlgorithm/IndiansCoin.java
@@ -0,0 +1,27 @@
+package GreedyAlgorithm;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class IndiansCoin {
+    public static void main(String args[]){
+        Integer coins[] = {1,2,5,10,20,50,100,500,2000};
+        Arrays.sort(coins, Comparator.reverseOrder());
+        int count = 0;
+        int amount = 590;
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int i=0;i<coins.length;i++){
+            if(coins[i]<=amount){
+                while(coins[i]<=amount){
+                    count++;
+                    ans.add(coins[i]);
+                    amount = amount-coins[i];
+                }
+            }
+        }
+        System.out.println(count);
+        System.out.println(ans);
+
+    }
+}
